92111aa05dd653d92a3b561771239b38b7dab954
liambrophy <liam.brophy19@gmail.com>
2023-02-14 17:17:33 +0000

    day 1 complete

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import { Movie } from "../typings";
 
 interface Props {
+  // movie: Movie | DocumentData - For using FireBase
   movie: Movie
 }
 
 function Thumbnail({movie}: Props) {
   return (
-    <div className="relative h-28 min-w-[100px]">
+    <div className="relative h-28 min-w-[100px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ interface Props {
   documentaries: Movie[]
 }
 
-const Home = ({ 
+const Home = ({
   netflixOriginals,
   trendingNow,
   topRated,
@@ -25,19 +25,19 @@ const Home = ({
   horrorMovies,
   romanceMovies,
   documentaries,
-  }: Props) => {
+}: Props) => {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Header />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
-        <Banner netflixOriginals={netflixOriginals}/>
-        <section>
-        <Row title="Trending Now" movies={trendingNow} />
+        <Banner netflixOriginals={netflixOriginals} />
+        <section className='md:space-y-24'>
+          <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List */}
@@ -90,4 +90,4 @@ export const getServerSideProps = async () => {
   }
 }
 
-// left at 1:25:29
+// left at 2:11:36
