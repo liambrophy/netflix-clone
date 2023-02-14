1d241bc967c39f77afde310e5e7512b6f4af207a
liambrophy <liam.brophy19@gmail.com>
2023-02-14 13:57:57 +0000

    init commit

---
+++ components/Row.tsx
@@ -0,0 +1,34 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import { Movie } from "../typings";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  title: string
+  movies: Movie[]
+}
+
+function Row({ title, movies }: Props) {
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5]
+        transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon  className="leftRightArrows"/>
+
+
+        <div className="flex items-center space-x-5 overflow-x-scroll md:space-x-2.5 md:p-2">
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie}/>
+          ))}
+        </div>
+
+
+        <ChevronRightIcon className="leftRightArrows"/>
+      </div>
+    </div>
+  )
+}
+
+export default Row;
---
+++ components/Thumbnail.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Movie } from "../typings";
+
+interface Props {
+  movie: Movie
+}
+
+function Thumbnail({movie}: Props) {
+  return (
+    <div className="relative h-28 min-w-[100px]">
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+        alt=""
+      />
+    </div>
+  )
+}
+
+export default Thumbnail;
